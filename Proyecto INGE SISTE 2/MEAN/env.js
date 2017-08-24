09dfc92cf0226ad9dfd8fdea4793a74543fc95e9
CIUNACOTO <deejaymarlon506@hotmail.es>
2017-08-23 18:14:54 -0600

    CIUNACOTO
    nuevo proyecto carga inicial

---
+++ Proyecto INGE SISTE 2/MEAN/env.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+if (fs.existsSync('./public')) {
+  process.env.NODE_ENV = 'production';
+  process.env.databaseUri = 'mongodb://estandar:programacion@ds157723.mlab.com:57723/ciunacoto';
+  process.env.databaseName = 'production database: angular-2-app'; // Database name
+} else {
+  process.env.NODE_ENV = 'development';
+  process.env.databaseUri = 'mongodb://estandar:programacion@ds157723.mlab.com:57723/ciunacoto';
+  process.env.databaseName = 'development database: mean-angular-2'; // Database name
+}
