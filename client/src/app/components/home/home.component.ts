5d8f6aba8a74a67c03043789c68cfc6d60542dd1
Juan Gamboa <gamabasis@gmail.com>
2017-08-28 14:30:18 -0600

    Ajustes Imagen Portada y Color Navbar

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -6,7 +6,7 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit {  
 
   constructor(
     public authService: AuthService
